2433052847d0385755df712223641a07a301c6ab
JuiceyBeans <75553966+JuiceyBeans@users.noreply.github.com>
2024-10-10 22:11:09 +0100

    Optional tint

--- src/main/java/com/juiceybeans/toolery/api/item/armor/VanillaArmorLogic.java
+++ src/main/java/com/juiceybeans/toolery/api/item/armor/VanillaArmorLogic.java
@@ -3,10 +3,13 @@ package com.juiceybeans.toolery.api.item.armor;
 import com.google.common.collect.Multimap;
 import com.gregtechceu.gtceu.api.item.armor.ArmorComponentItem;
 import com.gregtechceu.gtceu.api.item.armor.IArmorLogic;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.juiceybeans.toolery.Toolery;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
@@ -18,10 +21,18 @@ import org.jetbrains.annotations.Nullable;
 public class VanillaArmorLogic implements IArmorLogic {
     private final ArmorItem.Type type;
     private final String armorTexture;
+    private final int tint;
+
+    public VanillaArmorLogic(ArmorItem.Type type, String armorTexture, int tint) {
+        this.type = type;
+        this.armorTexture = armorTexture;
+        this.tint = tint;
+    }
 
     public VanillaArmorLogic(ArmorItem.Type type, String armorTexture) {
         this.type = type;
         this.armorTexture = armorTexture;
+        this.tint = GTMaterials.Uraninite.getMaterialRGB();
     }
 
     @Override
@@ -31,14 +42,14 @@ public class VanillaArmorLogic implements IArmorLogic {
 
     @Override
     public int getArmorDisplay(Player player, @NotNull ItemStack armor, EquipmentSlot slot) {
-        return 0;
+        return 6;
     }
 
     @Override
     public @Nullable ResourceLocation getArmorTexture(ItemStack stack, Entity entity, EquipmentSlot slot, String type) {
         return slot != EquipmentSlot.LEGS ?
-                Toolery.id(String.format("textures/armor/%s_1.png", armorTexture)) :
-                Toolery.id(String.format("textures/armor/%s_2.png", armorTexture));
+                Toolery.id(String.format("textures/armor/%s_layer_1.png", armorTexture)) :
+                Toolery.id(String.format("textures/armor/%s_layer_2.png", armorTexture));
     }
 
     @Override
@@ -53,6 +64,8 @@ public class VanillaArmorLogic implements IArmorLogic {
 
     @Override
     public int getArmorLayerColor(ItemStack itemStack, int layerIndex) {
-        return IArmorLogic.super.getArmorLayerColor(itemStack, layerIndex);
+        return IArmorLogic.super.getArmorLayerColor(itemStack, tint);
     }
+
+
 }
--- src/main/java/com/juiceybeans/toolery/common/data/TooleryItems.java
+++ src/main/java/com/juiceybeans/toolery/common/data/TooleryItems.java
@@ -10,6 +10,7 @@ import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.armor.ArmorComponentItem;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.api.registry.registrate.GTRegistrate;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.common.item.armor.GTArmorMaterials;
 import com.juiceybeans.toolery.api.item.armor.VanillaArmorLogic;
 import com.tterrag.registrate.providers.ProviderType;
@@ -33,7 +34,7 @@ public class TooleryItems {
     public static ItemEntry<ArmorComponentItem> TEST_CHESTPLATE = TOOLERY_REGISTRATE
             .item("test_chestplate",
                     (p) -> new ArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.CHESTPLATE, p)
-                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.CHESTPLATE,"test")))
+                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.CHESTPLATE,"base")))
             .lang("Test Chestplate")
             .properties(p -> p.rarity(Rarity.UNCOMMON))
             .tag(Tags.Items.ARMORS_CHESTPLATES)
@@ -42,7 +43,7 @@ public class TooleryItems {
     public static ItemEntry<ArmorComponentItem> TEST_BOOTS = TOOLERY_REGISTRATE
             .item("test_boots",
                     (p) -> new ArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.BOOTS, p)
-                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.BOOTS,"test")))
+                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.BOOTS,"base")))
             .lang("Test Boots")
             .properties(p -> p.rarity(Rarity.UNCOMMON))
             .tag(Tags.Items.ARMORS_BOOTS)
@@ -51,7 +52,7 @@ public class TooleryItems {
     public static ItemEntry<ArmorComponentItem> TEST_LEGGINGS = TOOLERY_REGISTRATE
             .item("test_leggings",
                     (p) -> new ArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.LEGGINGS, p)
-                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.LEGGINGS,"test")))
+                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.LEGGINGS,"base")))
             .lang("Test Leggings")
             .properties(p -> p.rarity(Rarity.UNCOMMON))
             .tag(Tags.Items.ARMORS_LEGGINGS)
@@ -60,7 +61,7 @@ public class TooleryItems {
     public static ItemEntry<ArmorComponentItem> TEST_HELMET = TOOLERY_REGISTRATE
             .item("test_helmet",
                     (p) -> new ArmorComponentItem(GTArmorMaterials.ARMOR, ArmorItem.Type.HELMET, p)
-                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.HELMET,"test")))
+                            .setArmorLogic(new VanillaArmorLogic(ArmorItem.Type.HELMET,"base")))
             .lang("Test Helmet")
             .properties(p -> p.rarity(Rarity.UNCOMMON))
             .tag(Tags.Items.ARMORS_HELMETS)
